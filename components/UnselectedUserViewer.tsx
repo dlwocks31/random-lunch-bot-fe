152464e2c018efe8f67ed1873f15662d9dbf98b7
Jaechan <dlwocks31@gmail.com>
2022-05-21 20:07:36 +0900

    refactor: 더 간단한 상태로 처리

--- components/UnselectedUserViewer.tsx
+++ components/UnselectedUserViewer.tsx
@@ -3,9 +3,11 @@ import { SlackUser } from "../utils/slack/slack-user";
 
 export function UnselectedUserViewer({
   allUsers,
+  unselectedUsers,
   onChange,
 }: {
   allUsers: SlackUser[];
+  unselectedUsers: SlackUser[];
   onChange: (ids: string[]) => void;
 }) {
   return (
@@ -13,7 +15,12 @@ export function UnselectedUserViewer({
       <div> Unselected User: </div>
       <Select
         placeholder="유저 이름을 검색하세요"
+        hideSelectedOptions
         options={allUsers.map(({ id, displayName }) => ({
+          value: id,
+          label: displayName,
+        }))}
+        value={unselectedUsers.map(({ id, displayName }) => ({
           value: id,
           label: displayName,
         }))}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import { Button, FormControl, TextField } from "@mui/material";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import type { NextPage } from "next";
 import { useState } from "react";
 import { SendSlackMessage } from "../components/SendSlackMessage";
@@ -7,41 +6,38 @@ import { UnselectedUserViewer } from "../components/UnselectedUserViewer";
 import { UserGrouper } from "../components/UserGrouper";
 import { SlackUser } from "../utils/slack/slack-user";
 import { SlackService } from "../utils/slack/slack.service";
-
 const Home: NextPage = () => {
   const [oauthToken, setOauthToken] = useState("");
-  const [users, setUsers] = useState<SlackUser[]>([]);
-  const [selectedUsers, setSelectedUsers] = useState<SlackUser[]>([]);
+  const [users, setUsers] = useState<{ user: SlackUser; selected: boolean }[]>(
+    [],
+  );
   function getUsersFromSlack() {
     const slackService = new SlackService(oauthToken);
     slackService.findAllValidSlackUsers().then((users) => {
-      setUsers(users);
-      setSelectedUsers(users);
+      setUsers(users.map((u) => ({ user: u, selected: true })));
     });
   }
-  function getUnselectedUser() {
-    return users.filter((u) => !selectedUsers.includes(u));
-  }
   function unselectUser(id: string) {
-    setSelectedUsers(selectedUsers.filter((u) => u.id !== id));
+    setUsers((users) =>
+      users.map((u) => ({
+        user: u.user,
+        selected: u.user.id === id ? false : u.selected,
+      })),
+    );
   }
 
   function onUnselectUserChange(unselectedUserIds: string[]) {
-    setSelectedUsers(users.filter((u) => !unselectedUserIds.includes(u.id)));
+    const result = users.map((u) => ({
+      user: u.user,
+      selected: !unselectedUserIds.includes(u.user.id),
+    }));
+    setUsers(result);
   }
-  const columns: GridColDef[] = [
-    { field: "id", headerName: "ID", width: 90 },
-    {
-      field: "displayName",
-      headerName: "Name",
-      width: 150,
-    },
-    {
-      field: "email",
-      headerName: "Email",
-      width: 150,
-    },
-  ];
+
+  function getSelectedUsers() {
+    return users.filter((u) => u.selected).map((u) => u.user);
+  }
+
   return (
     <div className="form-root">
       <FormControl className="form-control">
@@ -54,11 +50,12 @@ const Home: NextPage = () => {
         />
         <Button onClick={getUsersFromSlack}>Submit</Button>
       </FormControl>
-      <div className="datagrid-container">
-        <DataGrid rows={users} columns={columns} />
-      </div>
-      <UserGrouper users={selectedUsers} unselectUserFn={unselectUser} />
-      <UnselectedUserViewer allUsers={users} onChange={onUnselectUserChange} />
+      <UserGrouper users={getSelectedUsers()} unselectUserFn={unselectUser} />
+      <UnselectedUserViewer
+        allUsers={users.map((u) => u.user)}
+        unselectedUsers={users.filter((u) => !u.selected).map((u) => u.user)}
+        onChange={onUnselectUserChange}
+      />
       <SendSlackMessage oauthToken={oauthToken} />
       <style jsx>{`
         .form-root {
