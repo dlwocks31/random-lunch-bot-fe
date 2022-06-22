4d898ba3c6798b4bff7c65585325892635c3d731
Jaechan <dlwocks31@gmail.com>
2022-06-23 02:41:45 +0900

    슬랙에서 상태 추출 간소화

--- utils/slack/GetGroupTypeHeuristics.ts
+++ utils/slack/GetGroupTypeHeuristics.ts
@@ -2,10 +2,13 @@ import { GroupType } from "../domain/GroupType";
 import { SlackUser } from "./slack-user";
 
 export function getGroupTypeHeuristics(user: SlackUser): GroupType {
-  return user.statusEmoji === ":palm_tree:" ||
-    user.statusMessage.includes("휴직")
+  // return user.statusEmoji === ":palm_tree:" ||
+  //   user.statusMessage.includes("휴직")
+  //   ? GroupType.EXCLUDED
+  //   : user.statusEmoji === ":house_with_garden:"
+  //   ? GroupType.REMOTE
+  //   : GroupType.OFFICE;
+  return user.statusMessage.includes("휴직")
     ? GroupType.EXCLUDED
-    : user.statusEmoji === ":house_with_garden:"
-    ? GroupType.REMOTE
     : GroupType.OFFICE;
 }
