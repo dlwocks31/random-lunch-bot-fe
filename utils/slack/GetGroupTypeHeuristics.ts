939238ebdc12800337c3eaf80395686d2a25b1f7
Jaechan <dlwocks31@gmail.com>
2022-08-20 23:54:27 +0900

    v1 전부 삭제!

--- utils/slack/GetGroupTypeHeuristics.ts
+++
@@ -1,9 +0,0 @@
-import { GroupType } from "../domain/GroupType";
-import { SlackUser } from "./slack-user";
-
-export function getGroupTypeHeuristics(user: SlackUser): GroupType {
-
-  return user.statusMessage.includes("휴직")
-    ? GroupType.EXCLUDED
-    : GroupType.OFFICE;
-}
