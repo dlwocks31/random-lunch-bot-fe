d57be3087592c5ba05b40468d567ce8b83b6fe6a
Jaechan <dlwocks31@gmail.com>
2022-08-21 21:41:20 +0900

    flex 연동 시, 어떤 유저가 재택과 제외 조에 추가되는지 명확히 표시

--- components/fetch/FlexUserFetcher.tsx
+++ components/fetch/FlexUserFetcher.tsx
@@ -1,5 +1,6 @@
 import { Alert, Button, TextField } from "@mui/material";
 import dayjs from "dayjs";
+import { map } from "lodash";
 import { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 
@@ -16,6 +17,7 @@ export function FlexUserFetcher({
   const [flexAid, setFlexAid] = useState("");
   const [date, setDate] = useState(dayjs().format("YYYY-MM-DD"));
   const [errorMessage, setErrorMessage] = useState("");
+  const [infoMessage, setInfoMessage] = useState("");
   const [fetchArgs, setFetchArgs] = useState({
     flexAid,
     date,
@@ -33,10 +35,17 @@ export function FlexUserFetcher({
     console.log({ data, isLoading });
     console.log("HIHI data changed");
     if (data) {
+      const userListDisplay = (list: any) => map(list, "name").join(", ");
       try {
+        const { timeOff, remoteWork } = data;
+        console.log({ timeOff, remoteWork });
+        const newInfoMessage = `제외에 추가됨: ${userListDisplay(
+          timeOff,
+        )} / 재택에 추가됨: ${userListDisplay(remoteWork)}`;
+        setInfoMessage(newInfoMessage);
         moveMembersByEmail(
-          data.timeOff.map((d: any) => d.email),
-          data.remoteWork.map((d: any) => d.email),
+          timeOff.map((d: any) => d.email),
+          remoteWork.map((d: any) => d.email),
         );
         setErrorMessage("");
       } catch (e) {
@@ -47,40 +56,57 @@ export function FlexUserFetcher({
 
   return hasUser ? (
     <div className="root">
-      <TextField
-        label="x-flex-aid"
-        value={flexAid}
-        onChange={(e) => setFlexAid(e.target.value)}
-        size="small"
-      />
-      <TextField
-        label="date"
-        value={date}
-        onChange={(e) => setDate(e.target.value)}
-        size="small"
-      />
-
-      {isLoading ? (
-        <Button disabled variant="outlined">
-          Loading..
-        </Button>
-      ) : (
-        <Button
-          variant="outlined"
-          onClick={() => setFetchArgs({ flexAid, date, queryTime: new Date() })}
+      <div className="form">
+        <TextField
+          label="x-flex-aid"
+          value={flexAid}
+          onChange={(e) => setFlexAid(e.target.value)}
           size="small"
-        >
-          플렉스 설정 가져오기
-        </Button>
-      )}
+        />
+        <TextField
+          label="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+          size="small"
+        />
+
+        {isLoading ? (
+          <Button disabled variant="outlined">
+            Loading..
+          </Button>
+        ) : (
+          <Button
+            variant="outlined"
+            onClick={() => {
+              setErrorMessage("");
+              setInfoMessage("");
+              setFetchArgs({ flexAid, date, queryTime: new Date() });
+            }}
+            size="small"
+          >
+            플렉스 설정 가져오기
+          </Button>
+        )}
+      </div>
       {errorMessage ? (
         <Alert severity="error" onClose={() => setErrorMessage("")}>
           {errorMessage}
         </Alert>
       ) : null}
+      {infoMessage ? (
+        <Alert severity="info" onClose={() => setInfoMessage("")}>
+          {infoMessage}
+        </Alert>
+      ) : null}
+
       <style jsx>{`
+        .form {
+          display: flex;
+          gap: 5px;
+        }
         .root {
           display: flex;
+          flex-direction: column;
           gap: 5px;
         }
       `}</style>
