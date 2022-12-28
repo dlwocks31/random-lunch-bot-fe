ff3330b1509cc883e1f2236a84810a84352d11c0
Jaechan <dlwocks31@gmail.com>
2022-12-28 19:12:37 +0900

    로그 메세지 추가

--- pages/api/slack/oauth.ts
+++ pages/api/slack/oauth.ts
@@ -12,9 +12,10 @@ export default async function handler(
   if (!user) {
     res.status(401).json({ error: "Unauthorized" });
   }
-
+  console.log("user", JSON.stringify(user, null, 2));
   const { data } = await supabase.from("slack_oauth_tokens").select().single();
 
+  console.log("data", JSON.stringify(data, null, 2));
   const teamName = data?.raw_oauth_response?.team.name;
   res.status(200).json({ teamName });
 }
