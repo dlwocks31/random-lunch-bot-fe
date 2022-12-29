5f2b4aac124cabd75a83ce998dae4429d7ad821f
Jaechan <dlwocks31@gmail.com>
2022-12-29 17:58:56 +0900

    Revert "슬랙 로그인 시 권한 가져오기 테스트"
    This reverts commit dd3322ddca61331d18e7d330cd9ed3c4417c1812.

--- components/auth/LoginDialog.tsx
+++ components/auth/LoginDialog.tsx
@@ -40,13 +40,6 @@ export function LoginDialog({
             supabaseClient.auth
               .signInWithOAuth({
                 provider: "slack",
-                options: {
-                  queryParams: {
-                    scope:
-                      "channels:join,chat:write,users:read,users:read.email,channels:read",
-                    granular_bot_scope: "1",
-                  },
-                },
               })
               .then(({ data, error }) => {
                 if (error) {
